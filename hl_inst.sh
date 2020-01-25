ccfbe7cc931c65b59fecbacd5540c407c44b631d
220242 <220242@gmail.com>
2020-01-25 21:47:06 +0200

    Update hl_inst.sh

--- hl_inst.sh
+++ hl_inst.sh
@@ -1,4 +1,3 @@
-echo PART 1++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++
 cd ~
 git clone --recursive https://github.com/FWGS/xash3d
 cd xash3d
@@ -16,7 +15,6 @@ mkdir halflife
 cp xash3d/build/engine/libxash.so halflife/
 cp xash3d/build/mainui/libxashmenu.so halflife/
 cp xash3d/build/game_launch/xash3d halflife/
-echo PART 2++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++
 git clone https://github.com/FWGS/hlsdk-xash3d
 cd hlsdk-xash3d
 mkdir build
@@ -30,18 +28,6 @@ cp hlsdk-xash3d/build/dlls/hl.so halflife/
 cd ~
 rm -rf hlsdk-xash3d
 rm -rf xash3d
-echo PART 3++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++
-echo Get the "valve" directory and its contents from your copy of HL.
-echo Copy it to the folder with your compiled Xash binaries.
-echo Go to the "valve" dir and open liblist.gam in a text editor.
-echo Change the line "gamedll_linux=<something>" to
-echo "gamedll_linux="hl.so""
-echo copy hl.so to /home/rock/halflife/valve/dlls/hl.so
-echo "That's all. "
-echo run game with "LD_LIBRARY_PATH=/optvc/lib:/home/rock/halflife/:. ./xash3d -console"
 
 
 
-#also u can add counter strike 1.6, just add "cstrike" folder to ~/halflife/ and
-#cp hl.so /cstrike/dlls/cs.so
-#and u can change game in halflife
