19f1997bb2e348b8b4ccfdc2b8fabc4699ac0c05
220242 <220242@gmail.com>
2020-06-16 15:58:49 +0300

    Update hl_inst.sh

--- hl_inst.sh
+++ hl_inst.sh
@@ -11,23 +11,18 @@ cd build
 cmake -DHL_SDK_PATH=../hlsdk/ -DXASH_VGUI=no -DXASH_NANOGL=yes -DXASH_GLES=yes -DXASH_64BIT=1 ..
 make
 cd ~
-mkdir halflife
-cp xash3d/build/engine/libxash.so halflife/
-cp xash3d/build/mainui/libxashmenu64.so halflife/
-cp xash3d/build/game_launch/xash3d halflife/
 git clone https://github.com/FWGS/hlsdk-xash3d
 cd hlsdk-xash3d
-mkdir build
-cd build
-python3 ../waf configure -T release --enable-goldsrc-support
-python3 ../waf
+python3 waf configure -T release
+python3 waf
 cd ~
-cp dlls/hl_arm64.so ~/halflife/
-cp cl_dll/client_arm64.so ~/halflife/
+mkdir halflife
+cp ~/xash3d/build/engine/libxash.so halflife/
+cp ~/xash3d/build/mainui/libxashmenu64.so halflife/
+cp ~/xash3d/build/game_launch/xash3d halflife/
+cp ~/hlsdk-xash3d/build/dlls/hl_arm64.so ~/halflife/
+cp ~/hlsdk-xash3d/build/cl_dll/client_arm64.so ~/halflife/client64.so
  #delete crap
 cd ~
 rm -rf hlsdk-xash3d
 rm -rf xash3d
-
-
-
