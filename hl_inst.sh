110eb95ae4fe4ec6e12928f8729136524759ae3f
220242 <220242@gmail.com>
2020-06-18 18:31:21 +0300

    cmplt

--- hl_inst.sh
+++ hl_inst.sh
@@ -1,28 +1,28 @@
 cd ~
-git clone --recursive https://github.com/FWGS/xash3d
-cd xash3d
+git clone --recursive https://github.com/FWGS/xash3d-fwgs
+cd xash3d-fwgs
 cd engine
 git clone https://github.com/FWGS/nanogl
 cd ..
-git clone https://github.com/ValveSoftware/halflife
-mv halflife hlsdk
-mkdir build
-cd build
-cmake -DHL_SDK_PATH=../hlsdk/ -DXASH_VGUI=no -DXASH_NANOGL=yes -DXASH_GLES=yes -DXASH_64BIT=1 ..
-make
+git clone https://github.com/ValveSoftware/halflife hlsdk/
+cd ~/xash3d-fwgs/ref_gl/nanogl
+git clone https://github.com/FWGS/nanogl/
+cd nanogl
+cd ~/xash3d-fwgs/
+python3 ./waf configure -T release --disable-vgui --enable-gles1 --enable-gles2 --64bits --enable-poly-opt
+python3 ./waf build
+sudo rm -rf /usr/local/lib/xash3d/*
+sudo python3 ./waf install
 cd ~
 git clone https://github.com/FWGS/hlsdk-xash3d
 cd hlsdk-xash3d
-python3 waf configure -T release
+python3 waf configure -T release --enable-goldsrc-support
 python3 waf
-cd ~
-mkdir halflife
-cp ~/xash3d/build/engine/libxash.so halflife/
-cp ~/xash3d/build/mainui/libxashmenu64.so halflife/
-cp ~/xash3d/build/game_launch/xash3d halflife/
+mkdir ~/halflife
+cp ~/xash3d-fwgs/build/game_launch/xash3d ~/halflife/
 cp ~/hlsdk-xash3d/build/dlls/hl_arm64.so ~/halflife/
-cp ~/hlsdk-xash3d/build/cl_dll/client_arm64.so ~/halflife/client64.so
+cp ~/hlsdk-xash3d/build/cl_dll/client_arm64.so ~/halflife/
  #delete crap
-cd ~
-rm -rf hlsdk-xash3d
-rm -rf xash3d
+rm -rf ~/hlsdk-xash3d
+rm -rf ~/xash3d-fwgs
+
